5ce0921eb30094742fcc51bcdd8cc6582bb049f0
Gregory Conrad <gregorysconrad@gmail.com>
2024-05-08 17:38:52 -0400

    Update configuration and info for change to wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,77 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = {}
+
+config.font = wezterm.font 'Hack'
+config.font_size = 14
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- We switch between just an opacity and a background image based on whether we are fullscreen
+function set_background(config, is_fullscreen)
+  if is_fullscreen then
+    config.window_background_opacity = nil
+    config.background = {
+      {
+        source = {
+          File = wezterm.home_dir .. '/.config/background.jpg',
+        },
+        attachment = { Parallax = 0.1 },
+        repeat_y = 'Mirror',
+        horizontal_align = 'Center',
+        opacity = 0.4,
+        hsb = {
+          hue = 1.0,
+          saturation = 0.95,
+          brightness = 0.5,
+        },
+      },
+    }
+  else
+    config.window_background_opacity = 0.85
+    config.background = nil
+  end
+end
+
+wezterm.on('window-resized', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  set_background(overrides, window:get_dimensions().is_full_screen)
+  window:set_config_overrides(overrides)
+end)
+
+config.native_macos_fullscreen_mode = true
+config.color_scheme = 'Catppuccin Mocha (Gogh)'
+
+config.keys = {
+  { key = 'Enter', mods = 'ALT', action = act.ToggleFullScreen, },
+  { key = 'q', mods = 'ALT', action = act.QuitApplication, },
+
+  { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left', },
+  { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right', },
+  { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down', },
+  { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up', },
+
+  { key = 'h', mods = 'SHIFT|ALT', action = act.AdjustPaneSize {'Left', 4}, },
+  { key = 'l', mods = 'SHIFT|ALT', action = act.AdjustPaneSize {'Right', 4}, },
+  { key = 'j', mods = 'SHIFT|ALT', action = act.AdjustPaneSize {'Down', 4}, },
+  { key = 'k', mods = 'SHIFT|ALT', action = act.AdjustPaneSize {'Up', 4}, },
+
+  { key = 'd', mods = 'ALT', action = act.SplitVertical, },
+  { key = 'r', mods = 'ALT', action = act.SplitHorizontal, },
+
+  { key = '[', mods = 'ALT', action = act.ActivateTabRelative(-1), },
+  { key = ']', mods = 'ALT', action = act.ActivateTabRelative(1), },
+
+  -- Floating panes (not implemented yet)
+    -- bind "Alt w" { ToggleFloatingPanes; }
+    -- bind "Alt e" { TogglePaneEmbedOrFloating; }
+    -- bind "Alt b" { MovePaneBackwards; }
+  -- Using defaults for tabs (CMD t, CMD 1-9)
+  -- Using defaults for find (CMD f, CTRL-r to toggle case sensitivity & regex modes) 
+  -- TODO opening scrollback may be an interesting idea, see:
+  -- https://wezfurlong.org/wezterm/config/lua/wezterm/on.html#predefined-events
+}
+
+return config
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # dotfiles
 - Managed with [yadm](https://yadm.io)
 - [Helix](https://helix-editor.com) for text/code editing
-- [Alacritty](https://alacritty.org) + [Zellij](https://zellij.dev) for terminal, with Hack font
-  - Alacritty is minimal by design, allowing it to pair nicely with a multiplexer like Zellij
+- [WezTerm](https://wezfurlong.org/wezterm/) for terminal with Hack font
+  - I like the configurability via Lua
 
 ## Changes
 - Used to run neovim and also VS Code + Vim keybindings, but Helix *just works* with no effort
@@ -12,3 +12,6 @@
 - Also tried [kitty](https://sw.kovidgoyal.net/kitty/) for a couple weeks
   - No builtin splits, admist a few other things
     - I'd rather my software do the bare minimum *very well* or be an all-encompassing package
+- After kitty tried [Alacritty](https://alacritty.org) + [Zellij](https://zellij.dev)
+  - Alacritty had some rough edges that I didn't like (was _too_ minimal)
+  - Zellij is modal, which I don't want for a multiplexer. I just want to hit Alt+Key and move on
