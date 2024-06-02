dcc10504df818dcea4060979f92cab7b89c39fc9
Gregory Conrad <gregorysconrad@gmail.com>
2024-06-01 23:46:08 -0400

    Add hostname to status bar and other improvements

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,20 +9,16 @@ config:set_strict_mode(true)
 config.font = wezterm.font 'Hack'
 config.font_size = 14
 config.color_scheme = 'Catppuccin Mocha (Gogh)'
-config.native_macos_fullscreen_mode = true
 
--- Switch between integrated tab bar and retro tab bar based on whether we are fullscreen
-function set_tab_bar_config(config, is_fullscreen)
-  if is_fullscreen then
-    config.window_decorations = nil
-    config.use_fancy_tab_bar = false
-    config.hide_tab_bar_if_only_one_tab = true
-  else
-    config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-    config.use_fancy_tab_bar = true
-    config.hide_tab_bar_if_only_one_tab = false
-  end
-end
+local TITLEBAR_COLOR = '#333333'
+config.native_macos_fullscreen_mode = true
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.window_frame = {
+  font = wezterm.font { family = 'Hack', weight = 'Bold' },
+  font_size = 13.0,
+  active_titlebar_bg = TITLEBAR_COLOR,
+  inactive_titlebar_bg = TITLEBAR_COLOR,
+}
 
 -- Switch between just an opacity and a background image based on whether we are fullscreen
 function set_background(config, is_fullscreen)
@@ -54,8 +50,56 @@ wezterm.on('window-resized', function(window, pane)
   local overrides = window:get_config_overrides() or {}
   local is_fullscreen = window:get_dimensions().is_full_screen
   set_background(overrides, is_fullscreen)
-  set_tab_bar_config(overrides, is_fullscreen)
   window:set_config_overrides(overrides)
+end)
+
+wezterm.on('update-status', function(window, pane)
+  local cells = {}
+
+  -- Figure out the hostname of the pane on a best-effort basis
+  local hostname = wezterm.hostname()
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri and cwd_uri.host then
+    hostname = cwd_uri.host
+  end
+  table.insert(cells, ' ' .. hostname)
+
+  -- Format date/time in this style: "Wed Mar 3 08:14"
+  local date = wezterm.strftime ' %a %b %-d %H:%M'
+  table.insert(cells, date)
+
+  -- Add an entry for each battery (typically 0 or 1)
+  local batt_icons = {'', '', '', '', ''}
+  for _, b in ipairs(wezterm.battery_info()) do
+    local curr_batt_icon = batt_icons[math.ceil(b.state_of_charge * #batt_icons)]
+    table.insert(cells, string.format('%s %.0f%%', curr_batt_icon, b.state_of_charge * 100))
+  end
+
+  -- Color palette for each cell
+  local text_fg = '#c0c0c0'
+  local colors = {
+    TITLEBAR_COLOR,
+    '#3c1361',
+    '#52307c',
+    '#663a82',
+    '#7c5295',
+    '#b491c8',
+  }
+
+  local elements = {}
+  while #cells > 0 and #colors > 1 do
+    local text = table.remove(cells, 1)
+    local prev_color = table.remove(colors, 1)
+    local curr_color = colors[1]
+
+    table.insert(elements, { Background = { Color = prev_color } })
+    table.insert(elements, { Foreground = { Color = curr_color } })
+    table.insert(elements, { Text = '' })
+    table.insert(elements, { Background = { Color = curr_color } })
+    table.insert(elements, { Foreground = { Color = text_fg } })
+    table.insert(elements, { Text = ' ' .. text .. ' ' })
+  end
+  window:set_right_status(wezterm.format(elements))
 end)
 
 config.keys = {
